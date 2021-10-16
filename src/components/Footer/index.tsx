e535c69f0bf34acf4370493f365aa761a0512834
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-16 13:04:59 -0300

    feat: Header and Footer structure

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./footer.scss"
+
+function Footer() {
+  return (
+    <footer>
+      Footer
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/contants/colors.ts
@@ -0,0 +1,6 @@
+export default {
+  colors: {
+    primary: "#e90000",
+    secondary: "#0e9000"
+  }
+}
