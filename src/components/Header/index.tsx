d645b573a721a813ac5b5b9c4650c8263b04e5f2
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-16 13:39:28 -0300

    feat: Menu structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import "./header.scss"
+import React from 'react';
+import './header.scss';
+
+import logo from '../../assets/logo-shadow-realista.png';
 
 function Header() {
   return (
     <header>
-      Header
+      <img src={logo} alt="Logo Milk & Fruit" />
+
+      <ul>
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Cupons</a>
+        </li>
+        <li>
+          <a href="#">Cardápio</a>
+        </li>
+        <li>
+          <a href="#">Sobre nós</a>
+        </li>
+        <li>
+          <a href="#">Restaurante</a>
+        </li>
+      </ul>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
