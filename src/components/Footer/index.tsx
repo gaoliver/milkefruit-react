13d0d43748bb8135a6eae0b68c7658ecf4a55505
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-16 18:28:15 -0300

    feat: Footer structure

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,9 +7,50 @@ function Footer() {
 
   return (
     <footer className="container-fluid">
-      <div className="row">
-        <div className="col-5 pages"></div>
-        <div className="col-5 pages"></div>
+      <div className="row justify-content-between">
+        <div className="col-5 pages">
+          <h5>Páginas</h5>
+          <ul>
+            <li>
+              <a href="#">Início</a>
+            </li>
+            <li>
+              <a href="#">Cupons</a>
+            </li>
+            <li>
+              <a href="#">Cardápio</a>
+            </li>
+            <li>
+              <a href="#">Restaurante</a>
+            </li>
+            <li>
+              <a href="#">Delivery</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-5 pages">
+          <h5>Institucional</h5>
+          <ul>
+            <li>
+              <a href="#">Sobre nós</a>
+            </li>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="#">Twitter</a>
+            </li>
+            <li>
+              <a href="#">WhatsApp</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="row justify-content-center">
         <div className="bottom">{`${year} • Milk & Fruit`} &reg;</div>
