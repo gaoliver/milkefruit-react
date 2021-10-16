6e9d39452b5a4b7d5eb6cf5f1ae7f6629c36cd24
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-16 16:20:35 -0300

    feat: Downer footer structured

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import "./footer.scss"
+import './footer.scss';
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <footer>
-      Footer
+    <footer className="container-fluid">
+      <div className="row">
+        <div className="col-5 pages"></div>
+        <div className="col-5 pages"></div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="bottom">{`${year} • Milk & Fruit`} &reg;</div>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
